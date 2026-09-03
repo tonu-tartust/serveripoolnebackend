6202645a0fd5f983f81684315e2bd8f532ed3bc9
Nivoracs <jasper.sten@outlook.com>
2026-09-03 09:50:20 +0300

    add math example

--- learnphp/index.php
+++ learnphp/index.php
@@ -1,4 +1,22 @@
 <?php
 $test = 10;
-// comment
+$test = 10 + 10;
+$test = 10 - 10;
+$test = 10 * 10;
+$test = 10 / 10;
+$test = $test + 10;
+$test = 2 ** 3;
+$test = 5 % 2;
+$test++;
+$test--;
+--$test;
+++$test;
+
+
+$a = 1;
+$b = ++$a;
+var_dump($b, $a);
+
+$test = sin(pi());
+
 ?>
