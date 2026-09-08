cbcf4e7a4de4a8d1aab0d104c484e30bc2847620
Nivoracs <jasper.sten@outlook.com>
2026-09-08 09:23:36 +0300

    add array example

--- learnphp/index.php
+++ learnphp/index.php
@@ -1,13 +1,21 @@
 <?php
-$test = 10 > 5;
-$test = 5 < 10;
-$test = 10 == 10;
-$test = 10 <= 10;
-$test = 10 >= 10;
-$test = 10 == '10';
-$test = 10 === '10';
-$test = 10 != '10';
-$test = 10 !== '10';
-$test = 0 == false;
+$test = array(1, 2, 3);
+$test = [1, 2, 3];
+$test = [1, 'sdsds', true, [1, 2, 3]];
+$test = [
+  'name' => 'tonu',
+  'age' => 33,
+  3,
+  'sdsds',
+  true,
+  100 => 'lol',
+  'troll',
+];
 
+var_dump($test['name']);
+array_push($test, 'added', 'dwdwdw', 3);
+$test[] = 'new value';
+$test['name'] = 'bob';
+$test[1] = 99999;
+unset($test[3]);
 var_dump($test);
