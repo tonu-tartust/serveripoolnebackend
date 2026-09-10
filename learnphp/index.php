b188255bac41396e85960237562032aace05166f
Nivoracs <jasper.sten@outlook.com>
2026-09-10 09:12:33 +0300

    add annomunus function example

--- learnphp/index.php
+++ learnphp/index.php
@@ -1,38 +1,19 @@
 <?php
 
-function hello(): void
-{
-  var_dump('Hello');
+$test = function(){
+  var_dump('shh');
 }
 
-var_dump(hello());
-hello();
+var_dump($test);
 
-function helloName($name = 'nameless', $age)
-{
-  var_dump("Hello, $name! You are $age years old!");
-}
+$test();
+call_user_func($test);
 
-helloName('tonu', 30);
+$numbers = [1,2,3,4,5];
 
-function square(int $a): int
-{
-  if ($a < 0) {
-    return 0;
-  }
-  return $a * $a;
-}
+$squares = array_map(function ($n){
+  return $n*$n;
+}, $numbers);
 
-$answer = square(2);
-var_dump($answer);
-var_dump(square(4));
-
-function recursion($i)
-{
-  if ($i < 10) {
-    var_dump($i);
-    recursion(++$i);
-  }
-}
-
-recursion(0);
+$squares = array_map(fn ($n)=> $n*$n, $numbers);
+var_dump($squares)
