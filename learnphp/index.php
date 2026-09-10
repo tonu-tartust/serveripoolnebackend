4459bddad055ecf0734178735eb4835ffa30cc4c
Nivoracs <jasper.sten@outlook.com>
2026-09-10 09:51:04 +0300

    add example for pointers and pass by reference

--- learnphp/index.php
+++ learnphp/index.php
@@ -14,17 +14,25 @@ class Box
   }
 }
 
-$box1 = new Box();
-$box1->width = 10;
-$box1->height = 10;
-$box1->length = 10;
-$box1->open();
-var_dump($box1);
+$num1 = 1;
+$num2 = &$num1;
+$num1 = 2;
+var_dump($num1, $num2);
 
-$box2 = new Box();
-$box2->width = 20;
-$box2->height = 20;
-$box2->length = 20;
-$box2->open();
-var_dump($box2);
-var_dump($box1);
+$box1 = new Box();
+$box1->width = 1;
+$box2 = clone $box1;
+$box1->width = 2;
+var_dump($box1, $box2);
+
+$numbers = [1, 2, 3, 4, 5];
+
+for ($i = 0; $i < count($numbers); $i++) {
+  $n = &$numbers[$i];
+  $n += 1;
+}
+
+foreach ($numbers as $n) {
+  $n += 1;
+}
+var_dump($numbers);
