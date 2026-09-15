3dd499cfab80d46c2faa3e508f42b98a456c4335
Nivoracs <jasper.sten@outlook.com>
2026-09-15 09:28:13 +0300

    add magic methots

--- learnphp/index.php
+++ learnphp/index.php
@@ -2,37 +2,64 @@
 
 class Box
 {
-  public $width;
-  public $height;
-  public $length;
+
   public $isOpen = false;
   public $hasBeenOpened = false;
+
+  public function __construct(public $width, public $hight, public $length)
+  {
+    var_dump('Object Created');
+  }
+
 
   public function open()
   {
     $this->isOpen = true;
   }
+
+  public function __call($name, $arguments)
+  {
+    var_dump($name, $args);
+  }
+
+  public function __set($name, $value)
+  {
+    var_dump($name, $value);
+  }
+
+  public function __get($name)
+  {
+    var_dump($name);
+    return 'cool value';
+  }
+
+  public function __invoke()
+  {
+    var_dump('im a function');
+  }
+
+  public function __toString()
+  {
+    return 'Im a Box';
+  }
+
+  public function __destruct()
+  {
+    var_dump('Object destroyed');
+  }
 }
 
-$num1 = 1;
-$num2 = &$num1;
-$num1 = 2;
-var_dump($num1, $num2);
-
-$box1 = new Box();
-$box1->width = 1;
-$box2 = clone $box1;
-$box1->width = 2;
-var_dump($box1, $box2);
-
-$numbers = [1, 2, 3, 4, 5];
-
-for ($i = 0; $i < count($numbers); $i++) {
-  $n = &$numbers[$i];
-  $n += 1;
+function test()
+{
+  $box2 = new Box(1, 2, 3);
 }
+test();
 
-foreach ($numbers as $n) {
-  $n += 1;
-}
-var_dump($numbers);
+$box1 = new Box(1, 2, 3);
+$box2 = $box1;
+$box1 = 1;
+var_dump($box1);
+echo $box1;
+$box1->meow = 'lol';
+var_dump($box->coolio);
+$box1();
