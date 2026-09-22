f065c2b88aec8a18a5c5371a79fd91909b2c59cc
Nivoracs <jasper.sten@outlook.com>
2026-09-22 09:56:19 +0300

    Add simple router script

--- learnphp/public/index.php
+++ learnphp/public/index.php
@@ -1,2 +1,23 @@
 <?php
-var_dump('hello');
+
+if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
+  return false;
+} // serve the requested resource as-is.
+
+function dump(...$values)
+{
+  echo '<pre>';
+  var_dump($_SERVER);
+  echo '</pre>';
+}
+
+switch ($_SERVER['REQUEST_URI']) {
+  case '/':
+    include __DIR__ . '/../views/index.php';
+    break;
+  case '/us':
+    include __DIR__ . '/../views/us.php';
+    break;
+  default:
+    echo '404';
+}
